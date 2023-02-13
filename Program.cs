ac82e724902955af0daea294a7e667a8d483b369
shulevskyi <dshulevskyi@kse.org.ua>
2023-02-13 23:09:47 +0200

    Testing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp3;
+using Microsoft.VisualBasic;
 
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
@@ -15,20 +16,56 @@ List<Point> path = new List<Point> {start, end};
 var map = generator.Generate();
 new MapPrinter().Print(map, path);
 
+
+var startNode = new Node(43, 12, );
+
+List<Node> openSet = new List<Node> {};
+HashSet<Node> closedSet = new HashSet<Node> {};
+openSet.Add(startNode);
+
+
+
+
 void GetShortestPath(string[,] map, Point start, Point end)
 {
 
-
-    for (int i = start.Column; i < map.GetLength(0); i++)
+    while (openSet.Count > 0)
     {
-        Point currentPoint = new Point(i, start.Row);
-        if (map[currentPoint.Column, currentPoint.Row] == ".")
-        {
-            map[currentPoint.Column, currentPoint.Row] = "-";
-        }
+        // current = point in openSet with the lowest f_cost
+        
+        
     }
+
 }
 
 
-GetShortestPath(map, start, end);
-new MapPrinter().Print(map, path);
+// Distance for current cell to the target cell
+static double DistanceEnd(Point current_cell, Point end_cell)
+{
+    var xDiff = end_cell.Column - current_cell.Column;
+    var yDiff = end_cell.Row - current_cell.Row;
+    var distance = Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2));
+    return distance;
+}
+
+
+// Distance from the current cell to the start point
+static double DistanceOrigin(Point current_cell, Point start_cell)
+{
+    var xDiff = current_cell.Column - start_cell.Column;
+    var yDiff = current_cell.Row - start_cell.Row;
+    var distance = Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2));
+    return distance;
+}
+
+//Console.Write(DistanceEnd(start, start));
+
+
+
+//GetShortestPath(map, start, end);
+//new MapPrinter().Print(map, path);
+
+public Node(int value)
+{
+    Value = value;
+}
